a85ad3621bd8dad8f9bbbda763be09de94f590aa
avedrenn <avedrenn@42.student.fr>
2024-03-11 19:15:16 +0100

    Start users

--- django/app/hello_django/settings.py
+++ django/app/hello_django/settings.py
@@ -20,21 +20,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get("SECRET_KEY")
-SECRET_KEY = "coucou"
+SECRET_KEY = os.environ.get("SECRET_KEY")
+#SECRET_KEY = "coucou"
 
-#DEBUG = bool(os.environ.get("DEBUG", default=0))
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", default=0))
+#DEBUG = True
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-#ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-DJANGO_ALLOWED_HOSTS="localhost 127.0.0.1 [::1]"
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+#DJANGO_ALLOWED_HOSTS="localhost 127.0.0.1 [::1]"
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
 
 # Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -44,7 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'rest_framework',
-	'singlepage',       
+	'singlepage', 
+	'users',
+	'users.apps.UsersConfig',    
 ]
 
 MIDDLEWARE = [
